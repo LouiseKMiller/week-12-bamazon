48b5307a2802b346c287e1e5d0aca117386bfeae
LouiseKMiller <louise.austex@gmail.com>
2016-07-16 16:05:33 -0500

    complete logic for BamazonManager

--- BamazonManager.js
+++ BamazonManager.js
@@ -22,6 +22,20 @@ String.prototype.paddingLeft = function (paddingValue) {
 	return String(paddingValue + this).slice(-paddingValue.length);
 };
 
+function displayTable(tableData){
+ 	console.log("----------------------------------------------");
+ 	console.log("|  ID  |  Product Name  |  Price  | Quantity |");
+ 	console.log("----------------------------------------------");
+		for (var i = 0; i < tableData.length; i++) {
+			var idPadded = tableData[i].id.toString().paddingLeft("    ");
+			var namePadded = tableData[i].ProductName.paddingLeft("              ");
+			var pricePadded = ("$" + tableData[i].Price.toFixed(2)).toString().paddingLeft("       ");
+			var qtyPadded = tableData[i].StockQuantity.toString().paddingLeft("        ");
+    	console.log("| " + idPadded + " | " + namePadded + " | " + pricePadded + " | " + qtyPadded + " |");
+	}
+	console.log("----------------------------------------------");
+}
+
 
 connection.connect(function(err){
 	if (err) {
@@ -40,21 +54,99 @@ var showInventory = function(){
 			console.log('error on query: ' + err.stack);
 			return;
 		}
-     	console.log("----------------------------------------------");
-     	console.log("|  ID  |  Product Name  |  Price  | Quantity |");
-     	console.log("----------------------------------------------");
-   		for (var i = 0; i < res.length; i++) {
-   			var idPadded = res[i].id.toString().paddingLeft("    ");
-   			var namePadded = res[i].ProductName.paddingLeft("              ");
-   			var pricePadded = ("$" + res[i].Price.toFixed(2)).toString().paddingLeft("       ");
-        	console.log("| " + idPadded + " | " + namePadded + " | " + pricePadded + " | ");
-    	}
-    	console.log("-----------------------------------");
+     	displayTable(res);
     	start();
   	});
 
 }
 
+var showLowInventory = function(){
+	connection.query('SELECT * FROM products WHERE StockQuantity < 5',
+	function(err, res) {
+		if (err) {
+			console.log('error on query: ' + err.stack);
+			return;
+		}
+     	displayTable(res);
+    	start();
+  	});
+}
+
+var addQuantity = function() {
+    inquirer.prompt([{
+        name: "productID",
+        type: "input",
+        message: "Please enter the product ID"
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "Please enter the amount to add",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }]).then(function(answer) {
+    	connection.query('SELECT StockQuantity FROM Products WHERE id = ' +answer.productID, function(err, res) {
+    			var newQty = parseInt(res[0].StockQuantity) + parseInt(answer.quantity);
+
+		 		connection.query("UPDATE Products SET ? WHERE ?", [{
+            		StockQuantity: newQty
+            		}, {
+            		id: answer.productID
+        			}], function(err, res) {
+            			console.log("Your item was successfully updated!");
+            			start();
+            		});
+        	});
+    });
+}
+
+var addItem = function() {
+    inquirer.prompt([{
+        name: "product",
+        type: "input",
+        message: "Please enter the product name"
+    }, {
+        name: "department",
+        type: "input",
+        message: "Please enter the department name"
+    }, {
+        name: "price",
+        type: "input",
+        message: "Please enter the per item price",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },{
+        name: "quantity",
+        type: "input",
+        message: "Please enter the quantity",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }]).then(function(answer) {
+        connection.query("INSERT INTO Products SET ?", {
+            ProductName: answer.product,
+            DepartmentName: answer.department,
+            Price: answer.price,
+            StockQuantity: answer.quantity
+        }, function(err, res) {
+            console.log("Your item was added successfully!");
+            start();
+        });
+    })
+}
 
 function start() {
     inquirer.prompt({
@@ -70,13 +162,16 @@ function start() {
         		showInventory();
         		break;
     		case "View Low Inventory":
-    			console.log("View Low Inventory");
+    			console.log("");
+	    		showLowInventory();
     			break;
 			case "Add To Inventory":
-				console.log("add");
+				console.log("");
+				addQuantity();
 				break;
 			case "Add New Product":
-				console.log("product");
+				console.log("");
+				addItem();
 				break;
 			case "Exit":
 				console.log("exit");
